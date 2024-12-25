c6ec5ba36e541a59f89c6c5154c35d4570515416
Pragun <kakkarpragun.work@gmail.com>
2024-12-25 19:05:54 +0530

    added command line usage

--- email_sender.py
+++ email_sender.py
@@ -2,6 +2,11 @@ import smtplib
 from email.message import EmailMessage
 from string import Template
 from pathlib import Path
+import argparse
+
+parser = argparse.ArgumentParser(description='Send an email with a personalized template.')
+parser.add_argument("name",type=str,help="The name to be replaced in the template")
+args = parser.parse_args()
 
 html = Template(Path('index.html').read_text())
 email = EmailMessage()
@@ -9,7 +14,7 @@ email['from'] = 'Pragun Kakkar'
 email['to'] = 'kakkarpragun.work@gmail.com'
 email['subject'] = 'Test email'
 
-email.set_content(html.substitute(name='Pragun'), 'html')
+email.set_content(html.substitute(name=args.name), 'html')
 
 try:
     with smtplib.SMTP(host='smtp.gmail.com', port=587) as smtp:
